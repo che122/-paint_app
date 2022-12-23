824a6539c9dc96281f497726b98577275ee54089
che122 <106135369+che122@users.noreply.github.com>
2022-12-23 15:01:27 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,37 +1,3 @@
-// 면담 내용 :
-// 풍경에는 풍경에 어울리는 bgm 추천
-// 필터 추천 시 스케치처럼 보이게 해주는 필터 등 추가적 효과 보완 (색 필터만 씌우는 것 외에 다른 필터 필요)
-// 그래픽 강조. 예쁘게 포장해서 발표 (보여지는 게 그럴듯하게)
-// 시중에 나와있는 어플과 차별되는 포인트 적어도 2~3개 필요 (시장 조사)
-// 공기실에서 졸업작품 참고 영상 받기
-
-// 문제점 :
-// 1. Navigation - 내비게이션 기능이 한 번에 안되고 될 때도 있고 안 될 때도 있음
-// 2. Login 유지 - 앱 리로딩만 해도 로그인이 끊김
-// 3. Camera 문제 - 카메라에서 사진 선택 후 앱에 다시 들어올 때 리로딩되는 경우
-//    -> 처음 리로딩 문제는 해결, but 사진 재선택/갤러리로 이동하면 리로딩 발생
-//    -> No!!!! 다시 리로딩 문제....
-// 4. Gallery 분리 불가
-// - 갤러리 버튼 클릭 시 바로 사진 선택 후 다른 화면에 사진 출력 불가 (방법을 모름...)
-//   한 화면에 갤러리 버튼과 image가 동시에 존재해야 함
-//    -> 해결!!
-// 5. Calendar에서 markedDates 클릭하면 데이터 나오도록 agenda 사용 실패...
-// 6. FlatList에 데이터 로딩할 때 where로 uid가 유저의 것인 데이터만 로딩해야 함 (해결!)
-//    + 같은 날짜에 사진이 여러 장일 경우 합칠 수 없음...(역시 Agenda 필요...)
-// 7. Gallery 페이지 전체...component rendering이랑 이벤트 타이밍 맞추기...
-
-// 앞으로 할 일!!
-// 1. 사진 firebase에 저장 (완)
-// 2. 캘린더에서 firebase에 사진 저장한 날, 이미지 받아와서 해당 일 마킹하고 누르면 사진 띄우기 (타협..)
-// 3. 음악 추천 재생 (음악을 하드코딩으로 넣어줌...추천 필요)
-// 4. 친구 목록 해결..(구현 못하면 그냥 지우기) -> timeline..
-// 5. 기타 스타일 손보기
-// 6. Calendar화면에서 하단 Flatlist에 음악 재생도 가능하게 하고 싶다....
-
-// REACT_APP_CLIENT_ID='6c24124e1da04448bf18452649da81fe'
-// REACT_APP_CLIENT_SECRET='ee6f3f6142304901acc551291588bf98'
-// https://auth.expo.io/@che122/paint_application
-
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
